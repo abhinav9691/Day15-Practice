cb973b9f15b85a17c4364f2081cdbf104f3aa93e
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-19 00:36:09 +0530

    [Abhinav]-adding NearestNumber part

---
+++ NearestNumber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Day15_PracticeProblem
+{
+    public class NearestNumber
+    {
+        public NearestNumber()
+        {
+            Console.WriteLine("Hello World");
+        }
+        public NearestNumber(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public int Number(int a)
+        {
+            return a * a;
+        }
+        public static void FindType()
+        {
+            Type type = typeof(NearestNumber);
+            ConstructorInfo con = type.GetConstructor(Type.EmptyTypes);
+            NearestNumber nearest = (NearestNumber)con.Invoke(null);
+            object numberclassobject = con.Invoke(new object[] { });
+            MethodInfo method = type.GetMethod("Number");
+            object value = method.Invoke(numberclassobject, new object[] { 20 });
+            Console.WriteLine(nearest);
+            Console.WriteLine("NearestNumber.Number Returned {0}", value);
+        }
+        public static void ReflectionTest()
+        {
+            Type type = typeof(NearestNumber);
+
+            Console.WriteLine("Full name {0} ", type.FullName);
+
+            Console.WriteLine("Class Name {0} ", type.Name);
+            Console.WriteLine("Methods In Given Class");
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + "  " + method.Name);
+            }
+
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + "  " + property.Name);
+            }
+
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Day15_PracticeProblem
+{
+    public class Program
+    {
+        static bool evendigits(int number)
+        {
+            int rem = 0;
+            while (number > 0)
+            {
+                rem = number % 10;
+                if (rem % 2 != 0)
+                {
+                    return false;
+                }
+                number = number / 10;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number ");
+            int number = int.Parse(Console.ReadLine());
+            int num1 = number, num2 = number;
+            while (!evendigits(num1))
+            {
+                num1 -= 1;
+            }
+            while (!evendigits(num2))
+            {
+                num2 += 1;
+            }
+            int diff1 = number - num1;
+            int diff2 = number - num2;
+            int lowestnum = num1;
+            if (diff1 > diff2)
+            {
+                lowestnum = num2;
+            }
+            Console.WriteLine(lowestnum);
+            NearestNumber.FindType();
+            NearestNumber.ReflectionTest();
+        }
+    }
+}
